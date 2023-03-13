840d7a388a328816564fc3f02582caf04da087c9
Rohan Surve <rohansurve212@gmail.com>
2023-03-13 17:41:07 -0400

    UI-4 Chats List

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+// Import our components
+import ChatsScreen from './src/screens/ChatsScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ChatsScreen />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 50,
   },
-});
+})
---
+++ src/screens/ChatsScreen.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import { View, Text, FlatList } from 'react-native'
+
+// Import own components
+import ChatListItem from '../components/ChatListItem'
+
+// Import dummy data
+import chats from '../../assets/data/chats.json'
+
+const ChatsScreen = () => {
+  return (
+    <FlatList
+      data={chats}
+      renderItem={({ item }) => <ChatListItem chat={item} />}
+    />
+  )
+}
+
+export default ChatsScreen
